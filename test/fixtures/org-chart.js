4c090a0ab17c6f8e6fe346d7a20f38acf609ef1f
Wilson29thID <wilson@29th.org>
2017-11-24 16:19:35 -0500

    Use template string for org-chart fixture instead of mustache

--- test/fixtures/org-chart.js
+++ test/fixtures/org-chart.js
@@ -1,144 +1,51 @@
-const knex = require('knex')({ client: 'pg' })
+const ids = {
+  privateAxe: 1,
+  sergeantBug: 2,
+  lieutenantCup: 3,
+  technicianDew: 4,
+  bnhq: 1,
+  able: 2,
+  ap1: 3,
+  ap1s1: 4,
+  rifleman: 1,
+  squadLeader: 2,
+  platoonLeader: 3,
+  platoonClerk: 4
+}
+
+const createOrgChart = `
+  insert into public.user (id, first_name, last_name) values
+    (${ids.privateAxe}, 'Private', 'Axe'),
+    (${ids.sergeantBug}, 'Sergeant', 'Bug'),
+    (${ids.lieutenantCup}, 'Lieutenant', 'Cup'),
+    (${ids.technicianDew}, 'Technician', 'Dew');
+
+  insert into public.unit (id, abbr, parent_path) values
+    (${ids.bnhq}, 'Bn HQ', 'root'),
+    (${ids.able}, 'Able', 'root.${ids.bnhq}'), -- root.1
+    (${ids.ap1}, 'AP1', 'root.${ids.bnhq}.${ids.able}'), -- root.1.2
+    (${ids.ap1s1}, 'AP1S1', 'root.${ids.bnhq}.${ids.able}.${ids.ap1}'); -- root.1.2.3
+
+  insert into public.position (id, name, access_level) values
+    (${ids.rifleman}, 'Rifleman', 'member'),
+    (${ids.squadLeader}, 'Squad Leader', 'leader'),
+    (${ids.platoonLeader}, 'Platoon Leader', 'leader'),
+    (${ids.platoonClerk}, 'Platoon Clerk', 'clerk');
+
+  insert into public.assignment (user_id, unit_id, position_id) values
+    (${ids.privateAxe}, ${ids.ap1s1}, ${ids.rifleman}),
+    (${ids.sergeantBug}, ${ids.ap1s1}, ${ids.squadLeader}),
+    (${ids.lieutenantCup}, ${ids.ap1}, ${ids.platoonLeader}),
+    (${ids.technicianDew}, ${ids.ap1s1}, ${ids.rifleman}),
+    (${ids.technicianDew}, ${ids.ap1}, ${ids.platoonClerk});
+
+  insert into public.permission (unit_id, access_level, ability) values
+    (${ids.ap1}, 'leader', 'add_promotion'),
+    (${ids.ap1}, 'clerk', 'add_event'),
+    (${ids.ap1s1}, 'member', 'view_event');
+`
 
 module.exports = {
-  createUsers,
-  createUnits,
-  createPositions,
-  createAssignments,
-  createPermissions
-}
-
-function createUsers (ids) {
-  return knex.insert([
-    {
-      id: ids.privateAxe,
-      first_name: 'Private',
-      last_name: 'Axe'
-    },
-    {
-      id: ids.sergeantBug,
-      first_name: 'Sergeant',
-      last_name: 'Bug'
-    },
-    {
-      id: ids.lieutenantCup,
-      first_name: 'Lieutenant',
-      last_name: 'Cup'
-    },
-    {
-      id: ids.technicianDew,
-      first_name: 'Technician',
-      last_name: 'Dew'
-    }
-  ])
-  .into('public.user')
-  .toString()
-}
-
-function createUnits (ids) {
-  return knex.insert([
-    {
-      id: ids.bnhq,
-      abbr: 'Bn HQ',
-      parent_path: `root`
-    },
-    {
-      id: ids.able,
-      abbr: 'Able',
-      parent_path: `root.${ids.bnhq}` // root.1
-    },
-    {
-      id: ids.ap1,
-      abbr: 'AP1',
-      parent_path: `root.${ids.bnhq}.${ids.able}` // root.1.2
-    },
-    {
-      id: ids.ap1s1,
-      abbr: 'AP1S1',
-      parent_path: `root.${ids.bnhq}.${ids.able}.${ids.ap1}` // root.1.2.3
-    }
-  ])
-  .into('public.unit')
-  .toString()
-}
-
-function createPositions (ids) {
-  return knex.insert([
-    {
-      id: ids.rifleman,
-      name: 'Rifleman',
-      access_level: 'member'
-    },
-    {
-      id: ids.squadLeader,
-      name: 'Squad Leader',
-      access_level: 'leader'
-    },
-    {
-      id: ids.platoonLeader,
-      name: 'Platoon Leader',
-      access_level: 'leader'
-    },
-    {
-      id: ids.platoonClerk,
-      name: 'Platoon Clerk',
-      access_level: 'clerk'
-    }
-  ])
-  .into('public.position')
-  .toString()
-}
-
-function createAssignments (ids) {
-  return knex.insert([
-    {
-      user_id: ids.privateAxe,
-      unit_id: ids.ap1s1,
-      position_id: ids.rifleman
-    },
-    {
-      user_id: ids.sergeantBug,
-      unit_id: ids.ap1s1,
-      position_id: ids.squadLeader
-    },
-    {
-      user_id: ids.lieutenantCup,
-      unit_id: ids.ap1,
-      position_id: ids.platoonLeader
-    },
-    {
-      user_id: ids.technicianDew,
-      unit_id: ids.ap1s1,
-      position_id: ids.rifleman
-    },
-    {
-      user_id: ids.technicianDew,
-      unit_id: ids.ap1,
-      position_id: ids.platoonClerk
-    }
-  ])
-  .into('public.assignment')
-  .toString()
-}
-
-function createPermissions (ids) {
-  return knex.insert([
-    {
-      unit_id: ids.ap1,
-      access_level: 'leader',
-      ability: 'add_promotion'
-    },
-    {
-      unit_id: ids.ap1,
-      access_level: 'clerk',
-      ability: 'add_event'
-    },
-    {
-      unit_id: ids.ap1s1,
-      access_level: 'member',
-      ability: 'view_event'
-    }
-  ])
-  .into('public.permission')
-  .toString()
+  createOrgChart,
+  ids
 }
