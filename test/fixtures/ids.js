c7260faf5d4d0e212115cf7fcdb3c1c15e59805e
Wilson29thID <wilson@29th.org>
2017-11-24 13:20:39 -0500

    Use sql template via mustache

---
+++ test/fixtures/ids.js
@@ -0,0 +1,14 @@
+module.exports = {
+  privateAxe: 1,
+  sergeantBug: 2,
+  lieutenantCup: 3,
+  technicianDew: 4,
+  bnhq: 1,
+  able: 2,
+  ap1: 3,
+  ap1s1: 4,
+  rifleman: 1,
+  squadLeader: 2,
+  platoonLeader: 3,
+  platoonClerk: 4
+}
