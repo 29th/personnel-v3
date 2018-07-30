012aaccb4a184731f131916f7907c3279d75deab
Wilson29thID <wilson@29th.org>
2018-07-30 06:49:57 -0400

    Add home page indicating login status

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+  helper_method :current_user
+
+  private
+    def current_user
+      begin
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+      rescue Exception => e
+        nil
+      end
+    end
+
 end
