2ae8c14ef304aa803bf5f2f857cf6ef8e0d5a315
wilson29thid <wilson@29th.org>
2020-05-17 22:41:27 +0100

    Populate units ancestry column from path

--- db/migrate/20200515175714_add_ancestry_to_units.rb
+++ db/migrate/20200515175714_add_ancestry_to_units.rb
@@ -2,5 +2,11 @@ class AddAncestryToUnits < ActiveRecord::Migration[6.0]
   def change
     add_column :units, :ancestry, :string
     add_index :units, :ancestry
+
+    sql = <<~EOF
+      UPDATE units
+      SET ancestry = NULLIF(TRIM(BOTH '/' FROM path), '')
+    EOF
+    Unit.connection.execute(sql)
   end
 end
