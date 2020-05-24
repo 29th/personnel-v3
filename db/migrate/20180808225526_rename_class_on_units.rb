5664ba190bf5edf8e17fbe109acfa2288ae76cc1
wilson29thid <wilson@29th.org>
2020-05-24 14:26:37 +0100

    Fix migration name

--- db/migrate/20180808225526_rename_class_on_units.rb
+++ db/migrate/20180808225526_rename_class_on_units.rb
@@ -1,4 +1,4 @@
-class RenameUsersClass < ActiveRecord::Migration[5.2]
+class RenameClassOnUnits < ActiveRecord::Migration[5.2]
   def change
     # `class` is a reserved word in ruby
     rename_column :units, :class, :classification
