34ec9183506a80120d1b63e90268eeba0b738788
wilson29thid <wilson@29th.org>
2020-05-24 16:20:10 +0100

    Show validation errors when factory tests fail

--- test/factories_test.rb
+++ test/factories_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class FactoriesTest < ActiveSupport::TestCase
   FactoryBot.factories.map(&:name).each do |factory_name|
     test "#{factory_name} factory is valid" do
-      assert build(factory_name).valid?
+      instance = build(factory_name)
+      assert instance.valid?, instance.errors.full_messages.inspect
     end
   end
 end
