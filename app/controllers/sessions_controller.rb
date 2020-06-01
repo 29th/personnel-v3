a82474783f13ec58baa5dc7e9f1108cdb4706a74
wilson29thid <wilson@29th.org>
2020-06-01 18:50:00 +0100

    Disable account creation if no user is found

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
 
   def create
     auth = request.env['omniauth.auth']
-    user = User.where(:steam_id => auth['uid']).first || User.create_with_auth(auth)
+    user = User.find_by_steam_id!(auth['uid'])
 
     reset_session
     session[:user_id] = user.id
