29ede7dff945804d8856f76b38d6da80a7c7bdb6
wilson29thid <wilson@29th.org>
2020-10-03 17:09:36 +0100

    Add games and latest videos to homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,7 @@ class HomeController < ApplicationController
     skip_authorization
 
     @splashes = Rails.configuration.homepage.splashes
+    @games = Rails.configuration.homepage.games
+    @playlists = Rails.configuration.homepage.playlists
   end
 end
