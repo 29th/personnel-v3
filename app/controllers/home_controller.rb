82083aae7f1b161795558bed81040879cb1feab1
wilson29thid <wilson@29th.org>
2020-10-03 17:12:30 +0100

    Add footer and use application helper for homepage config

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,5 @@
 class HomeController < ApplicationController
   def index
     skip_authorization
-
-    @splashes = Rails.configuration.homepage.splashes
-    @games = Rails.configuration.homepage.games
-    @playlists = Rails.configuration.homepage.playlists
   end
 end
