6c1ef4ea2755270305f2e8ddc2d2b183aadfb779
wilson29thid <wilson@29th.org>
2020-10-28 08:00:29 +0000

    Simplify permission_map in NotePolicy

--- app/policies/note_policy.rb
+++ app/policies/note_policy.rb
@@ -1,13 +1,7 @@
 class NotePolicy < ApplicationPolicy
   def self.permission_map
-    write = {
-      members_only: -> user {
-        user.has_permission?('note_view_sq') ||
-        user.has_permission?('note_view_pl') ||
-        user.has_permission?('note_view_co') ||
-        user.has_permission?('note_view_mp') ||
-        user.has_permission?('admin')
-      },
+    {
+      members_only: -> user { user.member? },
       squad_level: -> user {
         user.has_permission?('note_view_sq') ||
         user.has_permission?('note_view_pl') ||
@@ -36,18 +30,13 @@ class NotePolicy < ApplicationPolicy
         user.has_permission?('admin')
       }
     }
-
-    read = write.clone
-    read[:members_only] = -> user { user.member? }
-
-    { read: read, write: write }
   end
 
   class Scope < Scope
     def resolve
       permitted_access_levels = []
 
-      NotePolicy.permission_map[:read].map do |access_level, fn|
+      NotePolicy.permission_map.map do |access_level, fn|
         if fn.call(user)
           permitted_access_levels.append(access_level)
         end
@@ -62,16 +51,26 @@ class NotePolicy < ApplicationPolicy
   end
 
   def show?
-    self.class.permission_map[:read][record.access.to_sym]&.call(user)
+    self.class.permission_map[record.access.to_sym]&.call(user)
+  end
+
+  def new?
+    user && (user.has_permission?('note_view_sq') ||
+             user.has_permission?('note_view_pl') ||
+             user.has_permission?('note_view_co') ||
+             user.has_permission?('note_view_mp') ||
+             user.has_permission?('admin'))
   end
 
   def create?
-    self.class.permission_map[:write][record.access.to_sym]&.call(user) &&
+    new? &&
+    self.class.permission_map[record.access.to_sym]&.call(user) &&
     user != record.user
   end
 
   def update?
-    self.class.permission_map[:write][record.access.to_sym]&.call(user) &&
+    new? &&
+    self.class.permission_map[record.access.to_sym]&.call(user) &&
     (user == record.author || user&.has_permission?('admin'))
   end
 
