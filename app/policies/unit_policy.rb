6ca75d399215d1c6a889c54fdd7720e30e5553a5
wilson29thid <wilson@29th.org>
2020-11-06 08:10:32 +0000

    Use more consistent formatting of admin check policies

--- app/policies/unit_policy.rb
+++ app/policies/unit_policy.rb
@@ -8,14 +8,14 @@ class UnitPolicy < ApplicationPolicy
   end
 
   def create?
-    user and user.has_permission?('admin')
+    user&.has_permission?('admin')
   end
 
   def update?
-    user and user.has_permission?('admin')
+    user&.has_permission?('admin')
   end
 
   def destroy?
-    user and user.has_permission?('admin')
+    user&.has_permission?('admin')
   end
 end
