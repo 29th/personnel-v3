319bed3881d0364e761369d76d39c6388e4bed03
wilson29thid <wilson@29th.org>
2020-11-15 16:21:23 +0000

    Use generic env vars for session cookie

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,7 +39,7 @@ class ApplicationController < ActionController::Base
       return @current_user if defined? @current_user
 
       @current_user = begin
-        cookie_name = ENV['VANILLA_COOKIE_NAME']
+        cookie_name = ENV['FORUMS_COOKIE_NAME']
         token = cookies[cookie_name]
         decoded = JsonWebToken.decode(token)
         forum_member_id = decoded[:sub]
