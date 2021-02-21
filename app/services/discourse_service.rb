078f3af63f0149e1f1d6dc00a9a6fac0afec48ce
wilson29thid <wilson@29th.org>
2021-02-21 13:11:31 +0000

    Paginate discourse api role list

--- app/services/discourse_service.rb
+++ app/services/discourse_service.rb
@@ -3,14 +3,26 @@ class DiscourseService
   base_uri ENV['DISCOURSE_BASE_URL']
   headers 'Api-Key' => ENV['DISCOURSE_API_KEY']
   headers 'Api-Username' => 'system'
+  MAX_LOOP_REQUESTS = 10 # safety cap
 
   def get_roles()
-    response = self.class.get('/groups.json')
-    return if response.body.nil? || response.body.empty?
+    groups = {}
+    current_page = 0
+    total_group_count = nil
 
-    response['groups'].reduce({}) do |accum, role|
-      accum[role['id']] = role['name']
-      accum
+    while (groups.size.zero? || groups.size < total_group_count) && current_page < MAX_LOOP_REQUESTS
+      query = { page: current_page }
+      response = self.class.get('/groups.json', query: query)
+      return if response.body.nil? || response.body.empty?
+
+      groups = response['groups'].each_with_object(groups) do |role, accum|
+        accum[role['id']] = role['name']
+      end
+
+      total_group_count = response['total_rows_groups']
+      current_page += 1
     end
+
+    groups
   end
 end
