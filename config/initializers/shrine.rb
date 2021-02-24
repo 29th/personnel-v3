9547588327bbc96def455c85748e052c8587a984
wilson29thid <wilson@29th.org>
2021-02-24 06:46:51 +0000

    Fix tests with shrine uploads Uses in-memory storage for shrine during tests

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -1,20 +1,31 @@
 require 'shrine'
-require 'shrine/storage/s3'
 
-s3_opts = {
-  bucket: ENV['STORAGE_BUCKET'],
-  region: ENV['STORAGE_REGION'],
-  access_key_id: ENV['STORAGE_ACCESS_KEY_ID'],
-  secret_access_key: ENV['STORAGE_SECRET_ACCESS_KEY'],
-  endpoint: ENV['STORAGE_ENDPOINT'],
-  public: true
-}
+if Rails.env.test?
+  require 'shrine/storage/memory'
 
-Shrine.storages = {
-  cache: Shrine::Storage::S3.new(prefix: 'cache', **s3_opts),
-  store: Shrine::Storage::S3.new(prefix: 'personnel', **s3_opts)
-}
+  Shrine.storages = {
+    cache: Shrine::Storage::Memory.new,
+    store: Shrine::Storage::Memory.new
+  }
+else
+  require 'shrine/storage/s3'
+
+  s3_opts = {
+    bucket: ENV['STORAGE_BUCKET'],
+    region: ENV['STORAGE_REGION'],
+    access_key_id: ENV['STORAGE_ACCESS_KEY_ID'],
+    secret_access_key: ENV['STORAGE_SECRET_ACCESS_KEY'],
+    endpoint: ENV['STORAGE_ENDPOINT'],
+    public: true
+  }
+
+  Shrine.storages = {
+    cache: Shrine::Storage::S3.new(prefix: 'cache', **s3_opts),
+    store: Shrine::Storage::S3.new(prefix: 'personnel', **s3_opts)
+  }
+
+  Shrine.plugin :url_options, store: { host: ENV['STORAGE_PUBLIC_HOST'] }
+end
 
 Shrine.plugin :activerecord
 Shrine.plugin :cached_attachment_data
-Shrine.plugin :url_options, store: { host: ENV['STORAGE_PUBLIC_HOST'] }
