81ddec5eeb2316e14246bb50ba54cbf893eaf316
wilson29thid <wilson@29th.org>
2021-03-08 15:15:08 +0000

    Add discourse to UserAward forum_id enum

--- app/models/user_award.rb
+++ app/models/user_award.rb
@@ -6,11 +6,11 @@ class UserAward < ApplicationRecord
 
   enum forum_id: { phpbb: 'PHPBB',
                    smf: 'SMF',
-                   vanilla: 'Vanilla' }
+                   vanilla: 'Vanilla',
+                   discourse: 'Discourse' }
 
   validates :date, presence: true
   validates_date :date
   validates :forum_id, presence: true
-  validates :topic_id, presence: true
-  validates :topic_id, numericality: { only_integer: true }
+  validates :topic_id, presence: true, numericality: { only_integer: true }
 end
