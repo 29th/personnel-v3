e8bf389d627b7a49176812c53b87a6df09d06b19
wilson29thid <wilson@29th.org>
2021-03-09 08:00:19 +0000

    Add finance records to admin Closes 29th/personnel#512

---
+++ app/admin/finance_records.rb
@@ -0,0 +1,82 @@
+ActiveAdmin.register FinanceRecord do
+  includes user: :rank
+
+  permit_params :date, :member_id, :forum_id, :topic_id, :notes,
+                :amount_received, :amount_paid, :fee, :vendor
+
+  scope :all, default: true
+  scope :income
+  scope :expenses
+
+  filter :user, collection: -> { User.active.includes(:rank).order(:last_name) }
+  filter :vendor, as: :select, collection: -> { FinanceRecord.vendors.map(&:reverse) }
+  filter :date
+  filter :amount_received
+  filter :amount_paid
+
+  config.sort_order = 'date_desc'
+
+  sidebar :balance, only: :index do
+    span number_to_currency(FinanceRecord.balance)
+  end
+
+  index do
+    selectable_column
+    column :date
+    column :user
+    column :vendor
+    column :amount_received do |record|
+      number_to_currency(record.amount_received)
+    end
+    column :amount_paid do |record|
+      number_to_currency(record.amount_paid)
+    end
+    column :fee do |record|
+      number_to_currency(record.fee)
+    end
+    column :notes do |finance_record|
+      finance_record.notes.truncate 75, omission: '…'
+    end
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :date
+      row :user
+      row :vendor
+      row :amount_received do |record|
+        number_to_currency(record.amount_received)
+      end
+      row :amount_paid do |record|
+        number_to_currency(record.amount_paid)
+      end
+      row :fee do |record|
+        number_to_currency(record.fee)
+      end
+      row :forum_id
+      row :topic_id
+      row :notes
+    end
+  end
+
+  form do |f|
+    f.semantic_errors *f.object.errors.keys
+    f.inputs do
+      f.input :date
+      f.input :user, as: :select, collection: User.active.includes(:rank).order(:last_name)
+      f.input :vendor, as: :select, collection: FinanceRecord.vendors.map(&:reverse)
+      f.input :amount_received
+      f.input :amount_paid
+      f.input :fee
+      f.input :forum_id, as: :select, collection: FinanceRecord.vendors.map(&:reverse)
+      f.input :topic_id, label: 'Topic ID'
+      f.input :notes
+    end
+    f.actions
+  end
+
+  after_save do |finance_record|
+    finance_record.user.update_coat
+  end
+end
