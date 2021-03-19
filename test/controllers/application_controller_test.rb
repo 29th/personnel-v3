48dde8ec71734c86f13c950b9433520667c22f43
wilson29thid <wilson@29th.org>
2021-03-19 07:11:54 +0000

    Make /admin accessible to anyone with new? permission on a handful of objects Fixes #531

--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -14,4 +14,20 @@ class ApplicationControllerTest < ActionDispatch::IntegrationTest
     get root_url
     assert_select '#user-dropdown', /^Pvt\. Foo/
   end
+
+  test "admin pages should only be viewable to someone with :new access on one of the pages" do
+    rifleman = create(:user)
+    clerk = create(:user)
+    clerk_unit = create(:unit)
+    create(:permission, :elevated, abbr: 'awarding_add', unit: clerk_unit)
+    create(:assignment, :elevated, user: clerk, unit: clerk_unit)
+
+    sign_in_as(rifleman)
+    get admin_root_url
+    assert_response 302
+
+    sign_in_as(clerk)
+    get admin_root_url
+    assert_response :success
+  end
 end
