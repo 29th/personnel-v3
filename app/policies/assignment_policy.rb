0ef540642edcd21c08d9422e4974c21fc4361cef
wilson29thid <wilson@29th.org>
2021-03-19 07:36:49 +0000

    Update assignment tests and restrict creating/modifying assignments for oneself

--- app/policies/assignment_policy.rb
+++ app/policies/assignment_policy.rb
@@ -16,21 +16,24 @@ class AssignmentPolicy < ApplicationPolicy
     if !record.persisted? && !record.changed?
       new?
     else
-      user&.has_permission_on_unit?('assignment_add', record.unit ||
+      (user&.has_permission_on_unit?('assignment_add', record.unit ||
         user&.has_permission?('assignment_add_any')) ||
-        user&.has_permission?('admin')
+        user&.has_permission?('admin')) &&
+        record.user != user
     end
   end
 
   def update?
-    user&.has_permission_on_unit?('assignment_add', record.unit) ||
+    (user&.has_permission_on_unit?('assignment_add', record.unit) ||
       user&.has_permission?('assignment_add_any') ||
-      user&.has_permission?('admin')
+      user&.has_permission?('admin')) &&
+      record.user != user
   end
 
   def destroy?
-    user&.has_permission_on_unit?('assignment_delete', record.unit) ||
+    (user&.has_permission_on_unit?('assignment_delete', record.unit) ||
       user&.has_permission?('assignment_delete_any') ||
-      user&.has_permission?('admin')
+      user&.has_permission?('admin')) &&
+      record.user != user
   end
 end
