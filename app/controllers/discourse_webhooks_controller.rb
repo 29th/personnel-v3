0786165e80fdfc39e33728144452e5206d4795d8
wilson29thid <wilson@29th.org>
2021-03-30 07:19:47 +0100

    Return more specific status codes in discourse webhook handler, and skip authenticitiy token

--- app/controllers/discourse_webhooks_controller.rb
+++ app/controllers/discourse_webhooks_controller.rb
@@ -1,25 +1,30 @@
 class DiscourseWebhooksController < ApplicationController
   SECRET_KEY = ENV["DISCOURSE_WEBHOOKS_SECRET"]
-  before_action :verify_event_type!
-  before_action :verify_webhook_token!
+  before_action :verify_event_type
+  before_action :verify_webhook_token
+  skip_before_action :verify_authenticity_token
 
   def receive
     discourse_user_id = params["user"]["id"]
     discourse_email = params["user"]["email"]
 
-    matching_user = User.find_by_email(discourse_email)
-    matching_user&.update!(discourse_forum_member_id: discourse_user_id)
+    matching_user = User.find_by_email!(discourse_email)
+    matching_user.update!(discourse_forum_member_id: discourse_user_id)
 
     head :no_content
+  rescue ActiveRecord::RecordNotFound
+    head :not_found
+  rescue ActiveRecord::RecordInvalid
+    head :unprocessable_entity
   end
 
   private
 
-  def verify_event_type!
+  def verify_event_type
     head :no_content unless request.headers["X-Discourse-Event"] == "user_activated"
   end
 
-  def verify_webhook_token!
+  def verify_webhook_token
     event_signature = request.headers["X-Discourse-Event-Signature"]
     return head :unauthorized unless event_signature
 
--- test/controllers/discourse_webhooks_controller_test.rb
+++ test/controllers/discourse_webhooks_controller_test.rb
@@ -21,16 +21,22 @@ class DiscourseWebhooksControllerTest < ActionDispatch::IntegrationTest
     }
 
     @subject = create(:user, email: "so@so.com")
+
+    ActionController::Base.allow_forgery_protection = true
   end
 
-  test "should throw 401 if missing signature" do
+  teardown do
+    ActionController::Base.allow_forgery_protection = false
+  end
+
+  test "should return 401 if missing signature" do
     headers = @headers.except("X-Discourse-Event-Signature".to_sym)
     post discourse_webhooks_url, params: @body, headers: headers, as: :json
     assert_response :unauthorized
     assert_nil User.find_by_discourse_forum_member_id(@discourse_user_id)
   end
 
-  test "should throw 401 if signature invalid" do
+  test "should return 401 if signature invalid" do
     headers = @headers.merge({"X-Discourse-Event-Signature": "invalidsignature"})
     post discourse_webhooks_url, params: @body, headers: headers, as: :json
     assert_response :unauthorized
@@ -50,12 +56,19 @@ class DiscourseWebhooksControllerTest < ActionDispatch::IntegrationTest
     assert_equal @subject, User.find_by_discourse_forum_member_id(@discourse_user_id)
   end
 
-  test "should throw if another user already has that discourse_forum_member_id" do
+  test "should return 422 if another user already has that discourse_forum_member_id" do
     other_subject = create(:user, discourse_forum_member_id: @discourse_user_id)
-    assert_raises(ActiveRecord::RecordInvalid) do
-      post discourse_webhooks_url, params: @body, headers: @headers, as: :json
-    end
+    post discourse_webhooks_url, params: @body, headers: @headers, as: :json
+    assert_response :unprocessable_entity
     assert_equal other_subject, User.find_by_discourse_forum_member_id(@discourse_user_id)
+  end
+
+  test "should return 404 if no user found with that email" do
+    body = @body.deep_merge({"user": {"email": "new@user.com"}})
+    headers = @headers.merge({"X-Discourse-Event-Signature": sign(body)})
+    post discourse_webhooks_url, params: body, headers: headers, as: :json
+    assert_response :not_found
+    assert_nil User.find_by_discourse_forum_member_id(@discourse_user_id)
   end
 
   private
