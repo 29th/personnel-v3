c52e41854c99b19df2f60a925044dc461dba9b73
wilson29thid <wilson@29th.org>
2021-04-07 07:30:57 +0100

    Replace assignments_transfer_from JS with stimulus controller Fixes 29th/personnel#553

--- app/javascript/packs/active_admin.js
+++ app/javascript/packs/active_admin.js
@@ -2,8 +2,8 @@ import { Application } from 'stimulus'
 import { definitionsFromContext } from 'stimulus/webpack-helpers'
 import '@activeadmin/activeadmin'
 import 'activeadmin_addons'
+
 import '../stylesheets/active_admin'
-import '../src/assignments_transfer_from'
 
 const application = Application.start()
 const context = require.context('../src/controllers', true, /\.js$/)
