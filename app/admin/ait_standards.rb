123683a757761799bab8a9349fe82ddfba0a7e86
wilson29thid <wilson@29th.org>
2021-04-09 07:47:54 +0100

    Add AIT Standards to admin

---
+++ app/admin/ait_standards.rb
@@ -0,0 +1,28 @@
+ActiveAdmin.register AITStandard do
+  permit_params :weapon, :game, :badge, :description, :details
+
+  filter :weapon, as: :select, collection: -> { AITStandard.weapons.map(&:reverse) }
+  filter :game, as: :select, collection: -> { AITStandard.games.map(&:reverse) }
+  filter :badge, as: :select, collection: -> { AITStandard.badges.map(&:reverse) }
+
+  index do
+    selectable_column
+    column :weapon
+    column :game
+    column :badge
+    column :description
+    actions
+  end
+
+  form do |f|
+    f.semantic_errors(*f.object.errors.keys)
+    f.inputs do
+      f.input :weapon, as: :select, collection: AITStandard.weapons.map(&:reverse)
+      f.input :game, as: :select, collection: AITStandard.games.map(&:reverse)
+      f.input :badge, as: :select, collection: AITStandard.badges.map(&:reverse)
+      f.input :description, as: :string
+      f.input :details
+    end
+    f.actions
+  end
+end
---
+++ app/models/ait_standard.rb
@@ -0,0 +1,17 @@
+class AITStandard < ApplicationRecord
+  self.table_name = "standards"
+  audited
+
+  enum weapon: {eib: "EIB", rifle: "Rifle", automatic_rifle: "Automatic Rifle",
+                machine_gun: "Machine Gun", armor: "Armor", sniper: "Sniper",
+                mortar: "Mortar", slt: "SLT", combat_engineer: "Combat Engineer",
+                submachine_gun: "Submachine Gun", pilot: "Pilot", grenadier: "Grenadier"}
+  enum game: {dh: "DH", rs: "RS", arma3: "Arma 3", rs2: "RS2", squad: "Squad"}
+  enum badge: {notapplicable: "N/A", marksman: "Marksman", sharpshooter: "Sharpshooter",
+               expert: "Expert"}
+
+  validates :weapon, presence: true
+  validates :game, presence: true
+  validates :badge, presence: true
+  validates :description, presence: true
+end
