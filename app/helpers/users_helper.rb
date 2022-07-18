347be7f16e735f6067a559c1d4e101c6b785498f
wilson29thid <wilson@29th.org>
2022-07-18 08:17:49 +0100

    Link to v2 user profile on event attendance

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+  def discourse_user_url(user)
+    base_url = Rails.configuration.endpoints[:discourse][:base_url][:external]
+    "#{base_url}/user-by-id/#{user.forum_member_id}/summary"
+  end
+
+  def v2_user_url(user)
+    base_url = Rails.configuration.endpoints[:personnel_v2_app][:base_url][:external]
+    "#{base_url}/#members/#{user.id}"
+  end
 end
