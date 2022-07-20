f1fee9b5e0f8c7ba77755c91ffc5f0298fc1b4a0
wilson29thid <wilson@29th.org>
2022-07-20 08:13:52 +0100

    Add ET suffix to times on event pages This is a temporary fix until we can show events in users' local timezones

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -12,4 +12,17 @@ module EventsHelper
       week_calendar(options, &block)
     end
   end
+
+  # Event timestamps are stored in ET in the database.
+  # Rails assumes they're in UTC, and we've told rails
+  # *not* to convert them to ET (because they already
+  # are in ET). So we have to convert it to a string
+  # and *then* tack on the time zone, and then format
+  # the time zone as an abbreviation instead of offset.
+  # see: https://github.com/29th/personnel/issues/593
+  def format_timestamp(datetime)
+    datetime.strftime("%F %R") # removes time zone
+      .in_time_zone(Time.zone) # adds correct time zone
+      .strftime("%F %R %Z") # formats time zone as abbr
+  end
 end
