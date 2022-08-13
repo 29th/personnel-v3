a13978a6a50c0a8c0e9dab640a0ea620a0ffd60e
wilson29thid <wilson@29th.org>
2022-08-13 12:16:07 +0100

    Use helpers for discourse and personnel v2 urls

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def homepage_config
     Rails.configuration.homepage
   end
+
+  def discourse_url
+    Rails.configuration.endpoints[:discourse][:base_url][:external]
+  end
+
+  def personnel_v2_app_url
+    Rails.configuration.endpoints[:personnel_v2_app][:base_url][:external]
+  end
 end
