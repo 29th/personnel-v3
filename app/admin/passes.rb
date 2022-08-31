5ed5252d3752b3e2dd861bb27814fba8ad7306f9
wilson29thid <wilson@29th.org>
2022-08-31 07:53:54 +0100

    Add batch_create ActiveAdmin plugin for events and passes Closes 29th/personnel#610

--- app/admin/passes.rb
+++ app/admin/passes.rb
@@ -4,7 +4,11 @@ ActiveAdmin.register Pass do
   includes user: :rank
 
   permit_params :member_id, :recruit_id, :start_date, :end_date,
-    :type, :reason
+    :type, :reason, bulk_member_ids: []
+
+  batch_create by_param: :bulk_member_ids, max: 5 do |member_id|
+    @pass.member_id = member_id
+  end
 
   scope :active, default: true
   scope :all
@@ -26,7 +30,12 @@ ActiveAdmin.register Pass do
   form do |f|
     f.semantic_errors(*f.object.errors.attribute_names)
     f.inputs do
-      input :user, collection: User.for_dropdown
+      if f.object.persisted?
+        input :user, collection: User.for_dropdown
+      else
+        input :bulk_member_ids, collection: User.for_dropdown, label: "User(s)",
+          input_html: {multiple: true}
+      end
       input :start_date, as: :datepicker
       input :end_date, as: :datepicker
       input :type, as: :select, collection: Pass.types.map(&:reverse)
