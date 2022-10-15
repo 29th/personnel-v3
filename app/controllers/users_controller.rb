8370afd5a6a632e90610ecea31be61a2a64e262e
wilson29thid <wilson@29th.org>
2022-10-15 17:30:53 +0100

    Fix n+1 queries on user profile and service record

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,20 +3,22 @@ class UsersController < ApplicationController
 
   def show
     @user = User.friendly.find(params[:id])
+    @active_assignments = @user.assignments.active.includes(:unit, :position)
     @donated = FinanceRecord.user_donated(@user)
     authorize @user
   end
 
   def service_record
     @user = User.friendly.find(params[:user_id])
+    @active_assignments = @user.assignments.active.includes(:unit, :position)
     authorize @user
 
-    user_awards = @user.user_awards.order(date: :desc)
-    promotions = @user.promotions.order(date: :desc)
-    assignments = @user.assignments.select("*, start_date AS date").order(start_date: :desc)
+    user_awards = @user.user_awards.includes(:award).order(date: :desc)
+    promotions = @user.promotions.includes(:new_rank, :old_rank).order(date: :desc)
+    assignments = @user.assignments.includes(:unit, :position).select("*, start_date AS date").order(start_date: :desc)
     demerits = @user.demerits.order(date: :desc)
     discharges = @user.discharges.order(date: :desc)
-    enlistments = @user.enlistments.order(date: :desc)
+    enlistments = @user.enlistments.includes(:unit).order(date: :desc)
 
     @items_by_year = (
       user_awards +
