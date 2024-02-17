3fb2b67b6d86256922f2620f22b113102f0d4c13
wilson29thid <wilson@29th.org>
2024-02-17 16:47:14 +0000

    Add new enlistment form Closes #237

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,14 @@
 // Entry point for the build script in your package.json
 
 import 'bootstrap.native/dist/bootstrap-native-v4'
+import { Application } from '@hotwired/stimulus'
+
+const application = Application.start()
+
+// Configure Stimulus development experience
+application.debug = false
+window.Stimulus = application
+
+import NestedForm from 'stimulus-rails-nested-form'
+
+application.register("nested-form", NestedForm)
