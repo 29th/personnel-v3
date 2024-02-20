310d4c947424c0d22a50650d6881504acd3a00bd
wilson29thid <wilson@29th.org>
2024-02-20 16:42:40 +0000

    Disable stimulus dev mode

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,7 +6,7 @@ import { Application } from '@hotwired/stimulus'
 const application = Application.start()
 
 // Configure Stimulus development experience
-application.debug = true
+application.debug = false
 window.Stimulus = application
 
 import NestedForm from 'stimulus-rails-nested-form'
