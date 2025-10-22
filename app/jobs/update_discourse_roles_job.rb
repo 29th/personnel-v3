309584f48098c7e7d56c1815338d8a22e7b686b2
wilson29thid <wilson@29th.org>
2025-10-22 06:53:28 +0100

    Fix method name in UpdateDiscourseRolesJob
    Fixes appsignal issue #111794

--- app/jobs/update_discourse_roles_job.rb
+++ app/jobs/update_discourse_roles_job.rb
@@ -6,7 +6,7 @@ class UpdateDiscourseRolesJob < ApplicationJob
     return unless user.forum_member_id.present?
 
     discourse_service = DiscourseService.new(user.forum_member_id)
-    expected_roles = user.forum_roles(:discourse)
+    expected_roles = user.forum_role_ids(:discourse)
     discourse_service.user.update_roles(expected_roles)
 
     if user.vanilla_forum_member_id.present?
