caae63b51963f6a3a18c0c32eb59c2d56cb34707
wilson29thid <wilson@29th.org>
2025-10-22 07:15:03 +0100

    Make GenerateMissingCoatsTask idempotent

--- app/tasks/maintenance/generate_missing_coats_task.rb
+++ app/tasks/maintenance/generate_missing_coats_task.rb
@@ -4,8 +4,11 @@ module Maintenance
   class GenerateMissingCoatsTask < MaintenanceTasks::Task
     def collection
       User
-        .where(id: User.active.select(:id))
-        .or(User.where(id: User.honorably_discharged.select(:id)))
+        .where(id: User.active.select(:id), service_coat_data: nil)
+        .or(
+          User.where(id: User.honorably_discharged.select(:id),
+            service_coat_data: nil)
+        )
     end
 
     def process(user)
